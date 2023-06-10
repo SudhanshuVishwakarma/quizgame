35dda9caa3fa121f7be9ca664e2cb012363a4af3
SudhanshuVishwakarma <vsudhanshu18@gmail.com>
2023-06-10 23:12:07 +0530

    guess the number

---
+++ Number_guesser/number_guesser.py
@@ -0,0 +1,37 @@
+import random
+
+top_of_range = input("type a number : ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print("Please type a number greater than a 0 ")
+        quit()
+else:
+    print("Type a number next time")
+    quit()
+
+
+random_number = random.randint(0, top_of_range)
+
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("make a guess : ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+
+    else:
+        print("Type a number next time")
+        continue
+
+    if user_guess == random_number:
+        print("You got it!")
+        break
+    elif user_guess > random_number:
+        print("you werer above the number")
+    else:
+        print("you were below the numeber")
+print("you got it in", guesses, "guesses")
